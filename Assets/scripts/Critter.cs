42e6ac4f68742ca5c602606476dd31b293592c5a
Dylan Christie <43067359+Argedco@users.noreply.github.com>
2018-09-21 14:16:57 +0100

    Added Score on Click

--- Assets/scripts/Critter.cs
+++ Assets/scripts/Critter.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Critter : MonoBehaviour {
 
     public Vector2 lowerRange;
-    public Vector2 upperRange; 
+    public Vector2 upperRange;
+
+    public Score scoreDisplay;
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +25,7 @@ public class Critter : MonoBehaviour {
     //Called when game object is clicked.
     void OnMouseDown()
     {
+        scoreDisplay.ChangeValue(1);
         Destroy(gameObject);
     }
 }
---
+++ Assets/scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    //Public variables that are visible in Unity
+    public TextMesh displayText;
+
+    //Private variables can't be touched by other scripts
+    private int currentValue = 0;
+
+    //update both the numerical data and the visual display
+    public void ChangeValue(int _toChange)
+    {
+        currentValue = currentValue + _toChange;
+        displayText.text = currentValue.ToString();
+
+    }
+	
+}
